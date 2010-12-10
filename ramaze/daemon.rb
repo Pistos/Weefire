110e660e1eb599dcfcfb3fd8481555586005a480
Pistos <gitsomegrace.5.pistos@geoshell.com>
2010-12-09 21:20:41 -0500

    Initial version.

---
+++ ramaze/daemon.rb
@@ -0,0 +1,60 @@
+require 'thread'
+require 'json'
+require 'ramaze'
+require 'tinder'
+
+load '/misc/git/weefire/weefire-config.rb'
+
+module Weefire
+  class Daemon
+    def initialize
+      @campfire = Tinder::Campfire.new( ::Weefire::CAMPFIRE_SUBDOMAIN, :token => ::Weefire::CAMPFIRE_API_TOKEN )
+      @room = @campfire.find_room_by_name( ::Weefire::CAMPFIRE_ROOM )
+      @unread = @room.transcript( Date.today ).map { |msg|
+        "<#{msg[:id]}> #{msg[:message]}"
+      }
+      @mutex = Mutex.new
+
+      Thread.new do
+        @room.listen do |msg|
+          $stderr.puts "Received: #{msg.inspect}"
+          @mutex.synchronize do
+            @unread << "<#{msg[:user][:name]}> #{msg[:body]}"
+          end
+        end
+      end
+    end
+
+    def unread
+      messages = []
+      @mutex.synchronize do
+        messages = @unread
+        @unread = []
+      end
+      messages
+    end
+
+    def recent
+      @room.transcript Date.today
+    end
+  end
+end
+
+$weefire_daemon = Weefire::Daemon.new
+
+class MainController < Ramaze::Controller
+  provide( :json, :type => 'application/json' ) { |action, value| value.to_json }
+
+  def index
+  end
+
+  def unread
+    $weefire_daemon.unread
+  end
+
+  def recent
+    $weefire_daemon.recent
+  end
+end
+
+Ramaze.start :adapter => :thin, :port => 7001
---
+++ weefire.rb
@@ -0,0 +1,34 @@
+require 'open-uri'
+require 'json'
+
+# callback for data received in input
+def buffer_input_cb( data, buffer, input_data )
+  Weechat::WEECHAT_RC_OK
+end
+
+# callback called when buffer is closed
+def buffer_close_cb( data, buffer )
+  Weechat::WEECHAT_RC_OK
+end
+
+def timer_cb( data, remaining_calls )
+  messages = JSON.parse( open( 'http://localhost:7001/unread.json' ).read )
+  messages.each do |m|
+    bputs m
+  end
+  Weechat::WEECHAT_RC_OK
+end
+
+def bputs( s )
+  Weechat.print $wf_buffer, s
+end
+
+def weechat_init
+  Weechat.register( "weefire", "Pistos", "0.1", "MIT", "Interface to Campfire (chat)", '', '' )
+
+  $wf_buffer = Weechat.buffer_new( "Weefire", "buffer_input_cb", "", "buffer_close_cb", "" )
+  Weechat.buffer_set( $wf_buffer, 'title', 'Weefire - Campfire Bridge' )
+  Weechat.hook_timer( 5 * 1000, 0, 0, "timer_cb", "" )
+
+  Weechat::WEECHAT_RC_OK
+end
